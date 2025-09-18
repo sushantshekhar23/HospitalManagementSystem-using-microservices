19a22f02dccbf53f264f9b9884286dad0a9af4d6
sushant shekhar <91068105+sushantshekhar23@users.noreply.github.com>
2025-09-18 23:05:22 +0530

    Delete HospitalManagementSystem-using-microservices-main/HospitalManagementSystem-using-microservices-main directory

--- HospitalManagementSystem-using-microservices-main/HospitalManagementSystem-using-microservices-main/patient-service/src/main/java/com/hms/patient_service/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hms.patient_service.repository;
-
-import com.hms.patient_service.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-}
